f02490f861f31492749b0e628fe46209c8b0045c
kalistenco <martinkalistenco@gmail.com>
2022-06-20 23:06:24 -0300

    Agrego tests en Main

--- tpFinalAdoo/src/main/java/Main.java
+++ tpFinalAdoo/src/main/java/Main.java
@@ -1,5 +1,14 @@
 import Gastos.Gasto;
 import Gastos.Recurrentes;
+import Notificaciones.Notificacion;
+import Notificaciones.Notificador;
+import Notificaciones.estrategias.EstrategiaDeNotificacion;
+import Notificaciones.estrategias.NotificacionPorEmail;
+import Notificaciones.estrategias.NotificacionPorSMS;
+import Notificaciones.estrategias.NotificacionPorWhatsApp;
+import Notificaciones.estrategias.adapters.email.AdapterEmailJavaEmail;
+import Notificaciones.estrategias.adapters.sms.AdapterSMSTwilio;
+import Notificaciones.estrategias.adapters.whatsapp.AdapterWhatsAppTwilio;
 
 import java.util.Date;
 
@@ -9,6 +18,20 @@ public class Main {
         Date date = new Date();
         Gasto gasto1 = new Recurrentes((float)1998,true, date,30);
 
+        Notificador notificador = new Notificador();
+        EstrategiaDeNotificacion notificadorSMS = new NotificacionPorSMS(new AdapterSMSTwilio());
+        EstrategiaDeNotificacion notificadorWhatsApp = new NotificacionPorWhatsApp(new AdapterWhatsAppTwilio());
+        EstrategiaDeNotificacion notificadorEmail = new NotificacionPorEmail(new AdapterEmailJavaEmail());
 
+        Notificacion notificacion = new Notificacion();
+        notificacion.setEmailDestinatario("martin@gmail.com");
+        notificacion.setEmailRemitente("tomas@gmail.com");
+        notificacion.setMensaje("Hola");
+        notificacion.setNroDestinatario("1234");
+        notificacion.setNroRemitente("9876");
+
+        notificador.setEstrategia(notificadorEmail);
+
+        notificador.enviar(notificacion);
     }
 }
