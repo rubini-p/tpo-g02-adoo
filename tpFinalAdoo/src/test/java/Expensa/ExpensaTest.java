2b30b68062708c03164a440d1c557a2d1f076893
gonbu <49691754+Gonzalo-Busson@users.noreply.github.com>
2022-06-21 14:21:36 -0300

    test exénsas

--- tpFinalAdoo/src/test/java/Expensa/ExpensaTest.java
+++ tpFinalAdoo/src/test/java/Expensa/ExpensaTest.java
@@ -1,6 +1,16 @@
 package Expensa;
 
+import Criterios.Criterio;
+import Criterios.PagoCompletoGenerarFFR;
+import Criterios.pagoCompleto;
+import Gastos.Gasto;
+import Gastos.Normales;
+import Gastos.Recurrentes;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,10 +18,38 @@ class ExpensaTest {
 
     @Test
     void setGastos() {
+        Date date = new Date();
+        Gasto gasto1 = new Recurrentes((float)1998, date,30);
+
+        List<Gasto> gastos = new ArrayList<Gasto>();
+        Administrador admin = new Administrador();
+        Criterio critero = new pagoCompleto();
+
+        Expensa expensa = new Expensa(date, gastos,critero, TipoDeExpensas.ORDINARIAS,admin);
+        try{
+            expensa.agregarGasto(gasto1);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 
     @Test
     void setCriterio() {
+        Date date = new Date();
+        Gasto gasto1 = new Recurrentes((float)1998, date,30);
+
+        List<Gasto> gastos = new ArrayList<Gasto>();
+        Administrador admin = new Administrador();
+        Criterio critero = new pagoCompleto();
+
+        Expensa expensa = new Expensa(date, gastos,critero, TipoDeExpensas.ORDINARIAS,admin);
+        try{
+            expensa.setCriterio(new PagoCompletoGenerarFFR());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Test
